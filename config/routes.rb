b740319b099a110bce8003c577a72c4ff3195b9e
huxiaoyang <huxiaoyang@udesk.cn>
2019-03-25 16:26:00 +0800

    capistrano

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :authors
+
+
+  get 'books/download/:id', to: 'books#download'
+
+
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
